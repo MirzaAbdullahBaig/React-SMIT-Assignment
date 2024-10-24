85d1efbd7d76b0dd3ce53605e7b6f47e1560ca0a
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-10-24 09:10:52 +0500

    React-Assignment

---
+++ Name Print Assignment-001/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Repetar - Mirza Abdullah Baig</h1>
+      <p className="read-the-docs">
+        React First SMIT Assignment
+      </p>
+    </>
+  )
+}
+
+export default App
