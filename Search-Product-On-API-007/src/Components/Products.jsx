d56c8a55265776291d0bd56a2cae3c79ad192642
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-11-22 22:44:13 +0500

    Search-Product-On-API-007

--- Search-Product-On-API-007/src/Components/Products.jsx
+++ Search-Product-On-API-007/src/Components/Products.jsx
@@ -1,16 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import { ShimmerPostItem } from "react-shimmer-effects";
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const API_KEY = `https://dummyjson.com/products`;
 
   const getProducts = async () => {
     try {
       const response = await axios.get(API_KEY);
-      console.log("🚀 ~ getProducts ~ response:", products);
-      setProducts(response.data.products);
+      setIsLoading(false);
+      setProducts(response?.data.products);
     } catch (error) {
       console.log(error);
     }
@@ -24,18 +26,28 @@ function Products() {
       <div className="mx-auto max-w-screen-xl px-4 md:px-8">
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {/* Product Card */}
-          {products.map((item) => {
-            return (
-              <ProductCard
-              key={item.id}
-              discountPercentage={item.discountPercentage}
-              thumbnail={item.thumbnail}
-              title={item.title}
-              price={item.price}
-              rating={item.rating}
-              />
-            )
-          })}
+          {isLoading ? (
+            <>
+              {Array.from({ length: 30 }).map(() => (
+                <div className="p-6">
+                  <ShimmerPostItem card title text cta imageHeight={190} Width={230} />
+                </div>
+              ))}
+            </>
+          ) : (
+            products.map((item) => {
+              return (
+                <ProductCard
+                  key={item.id}
+                  discountPercentage={item.discountPercentage}
+                  thumbnail={item.thumbnail}
+                  title={item.title}
+                  price={item.price}
+                  rating={item.rating}
+                />
+              );
+            })
+          )}
           {/* End Product Card */}
         </div>
       </div>
